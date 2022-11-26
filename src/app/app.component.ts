b0362cdbd12cab78bcf16efd89f6ef4d25b968d1
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-26 17:05:35 +0100

    feat: main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { LinkComponent } from './ui/components/link.component';
 
 @Component({
   selector: 'lab-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, LinkComponent],
+  imports: [CommonModule],
   template: `
     <header>
-      <nav>
-        <ul>
-          <strong>
-            <a routerLink="/"> {{ title }} </a>
-          </strong>
-        </ul>
-        <ul>
-          <a routerLink="/about">ℹ️ About us</a>
-        </ul>
-      </nav>
+      <a> {{ title }} </a>
     </header>
-    <router-outlet></router-outlet>
+    <article>
+      <header>
+        <h1>Angular Laboratory for <em>small</em> applications</h1>
+        <h2>Standalone edition</h2>
+      </header>
+      <main>
+        <ul>
+          <li>🏝️ Full Standalone APIs ✅</li>
+          <li>🗺️ Lazy Route Components ✅</li>
+          <li>📺 Container (page)✅ - Presenters (form, table...) ❌</li>
+          <li>🧪 E2E Testing ❌</li>
+        </ul>
+      </main>
+    </article>
     <footer>
-      <p>
-        ©️ {{ year }}
-        <lab-link
-          caption="Repository code on GitHub"
-          [href]="repoUrl"
-        ></lab-link>
-      </p>
+      <a [href]="repoUrl" target="_blank">Repository code on GitHub </a>
+      ©️ {{ year }} by <a [href]="authorUrl" target="_blank"> {{ author }} </a>
     </footer>
   `,
-  styles: [],
 })
 export class AppComponent {
   title = '🅰️ 🌱 🧫 angular-small-lab';
   repoUrl = 'https://github.com/AlbertoBasalo/angular-small-lab';
   year = new Date().getFullYear();
+  author = 'Alberto Basalo';
+  authorUrl = 'http://albertobasalo.dev';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)],
-});
+bootstrapApplication(AppComponent);

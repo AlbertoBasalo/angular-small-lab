a3dbcf28de8df4e752fbf2b62dabf01be66cb0b1
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-10 10:01:23 +0100

    test: intercept contact post

--- cypress/e2e/contact.cy.ts
+++ cypress/e2e/contact.cy.ts
@@ -1,6 +1,9 @@
 describe('The Contact page', () => {
   it('should send the contact form', () => {
     cy.visit('/contact');
+    const url = 'http://localhost:3000/contacts';
+    const response = { statusCode: 201, fixture: 'contact' };
+    cy.intercept('post', url, response).as('post_users');
     cy.fixture('contact').then((contact) => {
       cy.get('#name').clear().type(contact.name);
       cy.get('#email').clear().type(contact.email);

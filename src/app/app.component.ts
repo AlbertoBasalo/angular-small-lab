351a02cf4d3a7aafe2f6342da4a9045c52239b4a
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-10 09:36:00 +0100

    refactor: move layout into ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterBlock } from 'src/app/layout/footer.block';
-import { HeaderBlock } from 'src/app/layout/header.block';
+import { FooterBlock } from '@ui/layout/footer.block';
+import { HeaderBlock } from '@ui/layout/header.block';
 
 @Component({
   selector: 'lab-root',

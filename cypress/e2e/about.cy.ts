a117013396a5338c1f0536fa29b41c70d6e11616
Alberto Basalo <albertobasalo@hotmail.com>
2023-01-11 11:26:13 +0100

    test: about intercepted

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -1,5 +1,8 @@
 describe('The About page', () => {
   it('should work', () => {
+    cy.intercept('GET', 'http://localhost:3000/author', {
+      fixture: 'author',
+    }).as('getAbout');
     cy.visit('/about');
     cy.get('h3 > lab-link > a').contains('Alberto Basalo');
   });
